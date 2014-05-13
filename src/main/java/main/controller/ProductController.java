fb56663061fdd9d2105e0c77a4dad963f115a359
Sezin Karli <skarli@gmail.com>
2014-05-13 21:37:03 +0300

    spring security dependency and property added

--- src/main/java/main/controller/ProductController.java
+++ src/main/java/main/controller/ProductController.java
@@ -3,6 +3,7 @@ package main.controller;
 import main.dao.ProductRepository;
 import main.model.Product;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -32,6 +33,7 @@ public class ProductController {
         return "product-page";
     }
 
+    @Secured("ROLE_ADMIN")
     @RequestMapping(value = "/available")
     public String getSku(ModelMap modelMap){
         List<Product> availableProducts = repository.findByAvailabilityNot(0);
